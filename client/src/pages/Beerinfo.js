656ee98938d5c321321666a728d71f159b9c7f1e
ericshuanders <ericshuanders@gmail.com>
2020-10-25 14:57:38 -0400

    back button works on Beerinfo

--- client/src/pages/Beerinfo.js
+++ client/src/pages/Beerinfo.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 
 const Beerinfo = () => {
+  const history = useHistory();
+  const handleClick = () => {
+    history.goBack();
+  };
   return (
     <div>
-      <button>Back</button>
+      <button onClick={handleClick}>Back</button>
       <h1>Here's the Beer Info</h1>;
       <img />
       <p>Info out the beer</p>
