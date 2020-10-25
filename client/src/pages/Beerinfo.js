2bee8926f47fddbbee187d91e23ce9006131831d
ericshuanders <ericshuanders@gmail.com>
2020-10-25 15:33:15 -0400

    beer page displays beer info

--- client/src/pages/Beerinfo.js
+++ client/src/pages/Beerinfo.js
@@ -1,17 +1,32 @@
-import React from 'react';
-import { useHistory } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import { useHistory, useParams } from 'react-router-dom';
 
 const Beerinfo = () => {
   const history = useHistory();
   const handleClick = () => {
     history.goBack();
   };
+  const [beerInfo, setBeerInfo] = useState({});
+  const { id } = useParams();
+  useEffect(() => {
+    const getBeerInfo = async () => {
+      const result = await fetch(`https://api.punkapi.com/v2/beers/${id}`);
+      const data = await result.json();
+      setBeerInfo(data[0]);
+    };
+    try {
+      getBeerInfo();
+    } catch (error) {
+      alert('Error');
+    }
+  }, [id]);
+  console.log(beerInfo);
   return (
     <div>
       <button onClick={handleClick}>Back</button>
-      <h1>Here's the Beer Info</h1>;
-      <img />
-      <p>Info out the beer</p>
+      <h1>{beerInfo.name}</h1>;
+      <img src={beerInfo.image_url} />
+      <h2>{beerInfo.description}</h2>
     </div>
   );
 };
