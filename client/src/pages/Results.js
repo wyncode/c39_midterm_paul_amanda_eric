41293fddb1c0c7f448ebc3c4ee093d5e9e3f2297
ericshuanders <ericshuanders@gmail.com>
2020-10-26 19:31:25 -0400

    finished sorting project, now for some styling

--- client/src/pages/Results.js
+++ client/src/pages/Results.js
@@ -1,18 +1,24 @@
-// import React from 'react';
 import React, { useState, useEffect } from 'react';
-
 import { Row, Container } from 'react-bootstrap';
 import Card from 'react-bootstrap/Card';
+import classNames from 'classnames';
 import Searchbar from '../components/Searchbar';
 
 const Results = () => {
   const [search, setSearch] = useState();
   const [apiData, setApiData] = useState([]);
-  const [sort, setSort] = useState(false);
+  const [sortOptions, setSortOptions] = useState({
+    sortKey: 'abv',
+    sortDirection: 'asc'
+  });
 
   const handleSubmit = (event) => {
     event.preventDefault();
     setSearch(event.target.elements.searchbar.value);
+  };
+
+  const handleSortClick = (sortKey, sortDirection) => {
+    setSortOptions({ sortKey, sortDirection });
   };
 
   useEffect(() => {
@@ -30,17 +36,33 @@ const Results = () => {
     } catch (error) {
       console.log(error);
     }
-  }, [search, sort]);
+  }, [search]);
 
-  const handleSort = () => {
-    const newArray = apiData.sort(function (a, b) {
-      return a.abv - b.abv;
-    });
-    setSort(!sort);
-    // setApiData(newArray);
+  console.log('what are my values', {
+    stateObj: sortOptions,
+    keyIWant: sortOptions.sortKey,
+    directionIWant: sortOptions.sortDirection
+  });
+
+  const sortedBreweries = apiData.sort(function (a, b) {
+    const aValue = a[sortOptions.sortKey];
+    const bValue = b[sortOptions.sortKey];
+
+    // at the moment this will only work for numbers
+    // if you want to sort by strings, we will need to extend
+    // the logic to handle that
+    return sortOptions.sortDirection === 'asc'
+      ? aValue - bValue
+      : bValue - aValue;
+  });
+
+  const isActiveSortOption = (sortKey, sortDirection) => {
+    return (
+      sortKey === sortOptions.sortKey &&
+      sortDirection === sortOptions.sortDirection
+    );
   };
 
-  console.log(apiData);
   return (
     <div>
       <Searchbar submit={handleSubmit} />
@@ -48,10 +70,33 @@ const Results = () => {
       <Container>
         <h1>Let us paw you a drink</h1>
         {/* <Searchbar /> */}
-        <button onClick={handleSort}>Sort by ABV</button>
+        <button
+          className={classNames({ active: isActiveSortOption('abv', 'asc') })}
+          onClick={() => handleSortClick('abv', 'asc')}
+        >
+          Sort by ABV - ASC
+        </button>
+        <button
+          className={classNames({ active: isActiveSortOption('abv', 'desc') })}
+          onClick={() => handleSortClick('abv', 'desc')}
+        >
+          Sort by ABV - DESC
+        </button>
+        <button
+          className={classNames({ active: isActiveSortOption('ibu', 'asc') })}
+          onClick={() => handleSortClick('ibu', 'asc')}
+        >
+          Sort by IBU - ASC
+        </button>
+        <button
+          className={classNames({ active: isActiveSortOption('ibu', 'desc') })}
+          onClick={() => handleSortClick('ibu', 'desc')}
+        >
+          Sort by IBU - DESC
+        </button>
         <Row>
-          {apiData &&
-            apiData.map((beer) => (
+          {sortedBreweries &&
+            sortedBreweries.map((beer) => (
               <div>
                 <Card key={beer.id} style={{ width: 200, margin: 5 }}>
                   <a href={`/beers/${beer.id}`}>
